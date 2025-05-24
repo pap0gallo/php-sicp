410115da9eb5dd791b32f79403cc5b2daf3d2fa4
pap0gallo <khodunov14@gmail.com>
2025-05-24 20:30:15 +0300

    add fibonacci.php from lesson 5

--- fibonacci.php
+++ fibonacci.php
@@ -1,1 +1,13 @@
 <?php
+
+function fib($num)
+{
+    // BEGIN (write your solution here)
+    if ($num < 2) {
+        return $num;
+    }
+    return fib($num - 1) + fib($num - 2);
+    // END
+}
+
+print_r(fib(5));
